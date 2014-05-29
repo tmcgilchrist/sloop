b55f33a195d4870d218e0b581b7e6053f9267be5
Tim McGilchrist <timmcgil@gmail.com>
2014-05-29 17:43:24 +1000

    Message formats for RPC between nodes.
    Taken almost directly from the RAFT paper. I've added some extra sender
    ids to messages for debugging purposes.

--- include/sloop.hrl
+++ include/sloop.hrl
@@ -6,8 +6,8 @@
           timer :: timer:tref(),
 
 
-          self :: term(),           %% This node's id, registered name
-          members :: [pid()],        %% Pids of other cluster members
+          self         :: term(),         %% This node's id, registered name
+          members = [] :: [pid()],        %% Pids of other cluster members
 
 
           candidate_id :: string(), %% Candidate Requesting Vote
@@ -15,15 +15,33 @@
 
 }).
 
-
 %% RPC Message formats
 
 -record(request_vote, {
           term :: non_neg_integer(),
           candidate_id :: atom(),
           last_log_index :: non_neg_integer(),
-          last_log_term :: non_neg_integer()}).
+          last_log_term :: non_neg_integer()
+         }).
 
 -record(vote, {
+          id :: atom(),
           term :: non_neg_integer(),
-          vote_granted :: boolean()}).
+          vote_granted :: boolean()
+         }).
+
+
+-record(append_entries, {
+          term :: non_neg_integer(),
+          leader_id :: atom(),
+          prev_log_index :: non_neg_integer(),
+          prev_log_term :: non_neg_integer(),
+          entries,
+          commit_index :: non_neg_integer()
+         }).
+
+-record(append_response, {
+          id :: atom(),
+          term :: non_neg_integer(),
+          success :: boolean()
+         }).
